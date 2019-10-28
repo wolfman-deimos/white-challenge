306f5d671d008a9b02f96e47f0c0c75bddb40ac7
colinmarchbanks <marchbanks.colin@gmail.com>
2019-10-28 11:55:11 -0500

    Added purple ribbons(awareness for the homeless) to the foundations markers

---
+++ js/persons.js
@@ -0,0 +1,12 @@
+var examplePerson = {
+    firstName: '',
+    lastName: '',
+    age: '',
+    gender: '',
+    branch: '',
+    rank: '',
+    housing: '',
+    phone: '',
+    packageSelected: '',
+    currentLocation:''
+};
