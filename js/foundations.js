ff8ff1037fc4206a9495e934e27fe922828d0cf2
Eduardo Valerio Gamez <evaleriog@outlook.com>
2019-10-27 20:57:43 -0500

    Created JSOn File and test code to access it

--- js/foundations.js
+++ js/foundations.js
@@ -81,4 +81,19 @@ function getFoundationPhones(obj){
     return allPhones;
 }
 
+//Code to access the JSON File as a DB
+var requestURL = "http://localhost:3000/foundations";
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
+//code to manipulate the JSON FIle as it loads
+request.onload = function () {
+    var myFoundations = request.response;
+    console.log(myFoundations);
+}
+
+
+
 
