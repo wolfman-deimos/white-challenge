d69a66caf87df94800cd9765f9efca4fa5b1021e
Kenneth Howell <howell.kenneth.a@gmail.com>
2019-11-08 16:39:43 -0600

    popovers hiding with dimmed opacity

--- js/customjs.js
+++ js/customjs.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
     $('[data-toggle="popover"]').popover();
 
-    $('#redinfo').popover({content: "Combines above-average sized socks and seasonally appropriate gear with toiletries and hygiene products with additional packaged food and water", trigger:"hover"});
+
+
+    $('#redinfo').popover({content: "Combines above-average sized socks and seasonally appropriate gear with toiletries and hygiene products with additional packaged food and water", trigger: "hover"});
     $('#whiteinfo').popover({content: "Combines seasonally sensitive clothing and gear for average sizes with hygiene products and oral care products with additional food and water", trigger: "hover"});
     $('#blueinfo').popover({content: "Combines underclothing with seasonally appropriate gear with toiletries and hygiene products including feminine hygiene with additional food and water", trigger: "hover"});
 
@@ -23,17 +25,24 @@ $(document).ready(function(){
             if ($('#care_package').val() === "red") {
                 $("#redcard").fadeTo("slow", 1);
                 $("#whtcard, #bluecard").fadeTo("slow", .15);
+                $("#redinfo").popover("enable");
+                $('#blueinfo, #whiteinfo').popover("disable");
                 // $("#bluecard").fadeTo("slow", .15);
             } else if ($("#care_package").val() === "white") {
                 $("#whtcard").fadeTo("slow", 1);
                 $("#redcard, #bluecard").fadeTo("slow", .15);
+                $("#whiteinfo").popover("enable");
+                $('#redinfo, #blueinfo').popover("disable");
                 // $("#bluecard").fadeTo("slow", .15);
             } else if ($("#care_package").val() === "blue") {
                 $("#bluecard").fadeTo("slow", 1);
+                $("#blueinfo").popover("enable");
+                $('#redinfo, #whiteinfo').popover("disable");
                 $("#redcard, #whtcard").fadeTo("slow", .15);
                 // $("#whtcard").fadeTo("slow", .15);
             } else if ($("#care_package").val() === "none") {
                 $("#redcard, #whtcard, #bluecard").fadeTo("slow", 1);
+                $("#redinfo, #blueinfo, #whiteinfo").popover("enable");
             }
         }
 
